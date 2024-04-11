453c4ac3132ff88544085d552d6ce47be367b73e
seonyo <jsy6535@naver.com>
2024-04-11 11:40:43 +0900

    초기 커밋.

---
+++ CSBasic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic       //패키지 역할을 하는 게 namespace
+{
+    internal class Program  //internal은 접근제어자(C#의 기본 접근제어자는 private이기에 internal를 생략하면 실행 불가
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
